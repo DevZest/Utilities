5c295589ca4de6de57917ac8484986d7d0db70da
Weifen Luo <weifen.luo@devzest.com>
2018-05-30 15:23:37 +0800

    Add Argument.VerifyNotNull.

---
+++ src/Utilities/Argument.VerifyNotNull.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DevZest
+{
+    internal static partial class Extensions
+    {
+        internal static T VerifyNotNull<T>(T value, string parameterName)
+            where T : class
+        {
+            if (value == null)
+                throw new ArgumentNullException(parameterName);
+
+            return value;
+        }
+
+    }
+}
