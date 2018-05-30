99b888acd965af572d2d86e61615c452b26b309c
Weifen Luo <weifen.luo@devzest.com>
2018-05-30 22:44:53 +0800

    Add Type.ResolveStaticGetter.

--- src/Utilities/ToDo.cs
+++ src/Utilities/ToDo.cs
@@ -1,4 +1,6 @@
-﻿namespace DevZest
+﻿using System;
+
+namespace DevZest
 {
     internal static partial class Extensions
     {
@@ -12,6 +14,11 @@
             public static string ArgumentIsNullOrEmptyList(object parameterName)
             {
                 return string.Format("The argument '{0}' cannot be null or empty.", parameterName);
+            }
+
+            public static string CannotResolveStaticProperty(Type resourceType, string resourceName, Type propertyType)
+            {
+                return string.Format("Cannot resolve static property {0}.{1} of {2}.", resourceType, resourceName, propertyType);
             }
         }
     }
---
+++ src/Utilities/Type.ResolveStaticGetter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace DevZest
+{
+    internal static partial class Extensions
+    {
+        internal static Func<T> ResolveStaticGetter<T>(this Type resourceType, string resourceName, bool includingNonPublic = true)
+        {
+            Debug.Assert(resourceType != null);
+            Debug.Assert(!string.IsNullOrWhiteSpace(resourceName));
+
+            try
+            {
+                return BuildStaticGetter<T>(resourceType, resourceName,includingNonPublic);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ToDo.CannotResolveStaticProperty(resourceType, resourceName, typeof(T)), ex);
+            }
+        }
+
+        private static Func<T> BuildStaticGetter<T>(Type resourceType, string resourceName, bool includingNonPublic)
+        {
+            Debug.Assert(resourceType != null);
+            Debug.Assert(!string.IsNullOrWhiteSpace(resourceName));
+
+            var flags = BindingFlags.Static | BindingFlags.Public;
+            if (includingNonPublic)
+                flags = flags | BindingFlags.NonPublic;
+            PropertyInfo property = resourceType.GetProperty(resourceName, flags);
+            if (property == null)
+                return null;
+            var methodInfo = property.GetGetMethod(true);
+            if (methodInfo == null)
+                return null;
+            var call = Expression.Call(methodInfo);
+            return Expression.Lambda<Func<T>>(call).Compile();
+        }
+    }
+}
