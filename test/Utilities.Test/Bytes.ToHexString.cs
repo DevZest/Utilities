33115ac770e5693533764ad14caabdf43bf02f56
Weifen Luo <weifen.luo@devzest.com>
2018-05-30 15:09:05 +0800

    First commit.

---
+++ test/Utilities.Test/Bytes.ToHexString.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevZest
+{
+    [TestClass]
+    public partial class ExtensionsTests
+    {
+        [TestMethod]
+        public void Bytes_ToHexString()
+        {
+            var bytes = new byte[] { 5, 10, 127, 255 };
+            Assert.AreEqual("050A7FFF", bytes.ToHexString());
+        }
+    }
+}
