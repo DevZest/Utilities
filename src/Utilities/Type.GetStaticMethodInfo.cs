114c21f2643c1861be0fe02282fa2003001b3dd1
Weifen Luo <weifen.luo@devzest.com>
2018-05-31 01:14:52 +0800

    Add Nulable.EqualsTo/Type.GetStaticMethodInfo/Type.IsComparable/Type.IsNullable.

---
+++ src/Utilities/Type.GetStaticMethodInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace DevZest
+{
+    internal static partial class Extensions
+    {
+        internal static MethodInfo GetStaticMethodInfo(this Type type, string methodName, bool includingNonPublic = true)
+        {
+            var flags = BindingFlags.Static | BindingFlags.Public;
+            if (includingNonPublic)
+                flags = flags | BindingFlags.NonPublic;
+            return type.GetMethod(methodName, flags);
+        }
+    }
+}
---
+++ src/Utilities/Type.IsNullable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace DevZest
+{
+    internal static partial class Extensions
+    {
+        internal static bool IsNullable(this Type type)
+        {
+            return !type.GetTypeInfo().IsValueType || (Nullable.GetUnderlyingType(type) != null);
+        }
+    }
+}
