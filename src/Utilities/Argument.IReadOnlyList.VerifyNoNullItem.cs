8b51b13292ebfc9a785af8e9200520cacf6d8497
Weifen Luo <weifen.luo@devzest.com>
2018-05-31 09:00:00 +0800

    Add Argument.IReadOnlyList.VerifyNoNullItem; Rename: ArgumentList.VerifyNotEmpty -> Argument.IReadOnlyList.VerifyNotEmpty; ArgumentListItem.VerifyNotNull -> Argument.IReadOnlyListItem.VerifyNotNull; ArgumentString.VerifyNotEmpty -> Argument.String.VerifyNotEmpty.

---
+++ src/Utilities/Argument.IReadOnlyList.VerifyNoNullItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DevZest
+{
+    internal static partial class Extensions
+    {
+        internal static IReadOnlyList<T> VerifyNoNullItem<T>(this IReadOnlyList<T> list, string listParamName)
+            where T : class
+        {
+            if (list == null)
+                return list;
+
+            for (int i = 0; i < list.Count; i++)
+                list.VerifyNotNull(i, listParamName);
+
+            return list;
+        }
+
+    }
+}
