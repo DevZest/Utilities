8480e346c0a041a9c8698e1b239905594f98f96a
Weifen Luo <weifen.luo@devzest.com>
2019-06-10 17:11:20 +0800

    Add Decimal.TruncateTo.

---
+++ src/Utilities/Decimal.TruncateTo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DevZest
+{
+    internal static partial class Extensions
+    {
+        public static decimal TruncateTo(this decimal value, int precision)
+        {
+            decimal step = (decimal)Math.Pow(10, precision);
+            decimal tmp = Math.Truncate(step * value);
+            return tmp / step;
+        }
+    }
+}
---
+++ test/Utilities.Test/Decimal.TruncateTo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevZest
+{
+    [TestClass]
+    public class Decimal_TruncateTo
+    {
+        [TestMethod]
+        public void Test()
+        {
+            {
+                var x = 0.274M;
+                Assert.AreEqual(0.27M, x.TruncateTo(2));
+            }
+
+            {
+                var x = 0.275M;
+                Assert.AreEqual(0.27M, x.TruncateTo(2));
+            }
+        }
+    }
+}
